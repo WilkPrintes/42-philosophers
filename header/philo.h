c92ff5eda35c8f7725575e594dea159f193ff000
WilkPrintes <wilkp90@gmail.com>
2022-08-17 20:41:19 +0200

    testing mutex and threads

--- header/philo.h
+++ header/philo.h
@@ -6,7 +6,7 @@
 /*   By: wprintes <wprintes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 17:56:10 by wprintes          #+#    #+#             */
-/*   Updated: 2022/08/16 17:09:00 by wprintes         ###   ########.fr       */
+/*   Updated: 2022/08/17 19:55:25 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,24 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <pthread.h>
+#include <stdlib.h>
+
+typedef struct s_geral
+{
+	int		forks;
+	int		death;
+	time_t	t_sleep;
+	time_t	t_die;
+	time_t	t_eat;
+	time_t	t_init;
+	pthread_mutex_t	lock;
+} t_geral;
+
+typedef struct s_philo
+{
+	pthread_t		thread;
+	int				id;
+	int				eat;
+	t_geral			*geral;
+	time_t			last_eat;
+} t_philo;
--- src/philo.c
+++ src/philo.c
@@ -6,42 +6,115 @@
 /*   By: wprintes <wprintes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 17:55:28 by wprintes          #+#    #+#             */
-/*   Updated: 2022/08/16 17:05:05 by wprintes         ###   ########.fr       */
+/*   Updated: 2022/08/17 20:40:30 by wprintes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void *rotina_f1(void *parameters)
-{
-	struct timeval test;
-	time_t start;
-	time_t final;
-	
-	parameters = NULL;
-	gettimeofday(&test, NULL);
-	start = test.tv_usec + test.tv_sec * 1000000;
-	printf("philosopher %lu eating\n", pthread_self());
-	usleep(1000000);
-	printf("philosopher %lu sleeping\n", pthread_self());
-	usleep(1000000);
-	printf("philosopher %lu thinking\n", pthread_self());
-	usleep(1000000);
-	gettimeofday(&test, NULL);
-	final = test.tv_usec + test.tv_sec * 1000000;
-	printf("time passed of philo %lu: %ld\n", pthread_self(), final - start);
-	pthread_exit(0);
-}
+void init_geral(t_geral *geral);
+t_philo *create_philo(t_geral *geral, int count);
+time_t current_time(void);
+void *philo_comp(void *parameter);
 
 int main(void)
 {
-	pthread_t philo1;
-	pthread_t philo2;
-	int exit_code;
+	t_geral geral;
+	int count;
+	t_philo **philos;
 
-	pthread_create(&philo1, NULL, rotina_f1, NULL);
-	pthread_create(&philo2, NULL, rotina_f1, NULL);
-	exit_code = (int)pthread_join(philo1, NULL);
-	exit_code = (int)pthread_join(philo2, NULL);
-	printf("finalizado com code: %d\n", exit_code);
+	count = 0;
+	philos = malloc (sizeof(t_philo *) * 3);
+	init_geral(&geral);
+	while (count < 3)
+	{
+		philos[count] = create_philo(&geral, count);
+		count++;
+	}
+	philos[count] = NULL;
+	count = 0;
+	while (philos[count])
+	{
+		pthread_create(&philos[count]->thread, NULL, philo_comp, philos[count]);
+		count++;
+	}
+	while (1)
+	{
+		if (geral.death >= 1)
+			break;
+	}
+}
+
+void init_geral(t_geral *geral)
+{
+	geral->forks = 2;
+	geral->death = 0;
+	geral->t_eat = 500 * 1000;
+	geral->t_sleep = 1000 * 1000;
+	geral->t_die = 2000 * 1000;
+	geral->t_init = current_time();
+	pthread_mutex_init(&geral->lock, NULL);
+}
+
+void *philo_comp(void *parameter)
+{
+	t_philo *philo;
+
+	philo = (t_philo *)parameter;
+	while (1)
+	{
+		if (philo->geral->forks >= 2)
+		{
+			if ((current_time() - philo->last_eat) > philo->geral->t_die)
+			{
+				printf("philo %d died\n", philo->id);
+				pthread_mutex_lock (&philo->geral->lock);
+				philo->geral->death += 1;
+				pthread_mutex_unlock (&philo->geral->lock);
+				pthread_detach(philo->thread);
+				return (NULL);
+			}
+			printf("philo %d has taken a fork\n", philo->id);
+			pthread_mutex_lock (&philo->geral->lock);
+			philo->geral->forks -= 2;
+			printf("forks ok: %d\n", philo->geral->forks);
+			printf("philo %d is eating\n", philo->id);
+			usleep(philo->geral->t_eat);
+			philo->geral->forks += 2;
+			pthread_mutex_unlock (&philo->geral->lock);
+			philo->last_eat = current_time();
+			printf("philo %d is sleeping\n", philo->id);
+			usleep(philo->geral->t_sleep);
+			printf("philo %d is thinking\n", philo->id);
+		}else if ((current_time() - philo->last_eat) > philo->geral->t_die)
+		{
+			printf("philo %d died\n", philo->id);
+			pthread_mutex_lock (&philo->geral->lock);
+			philo->geral->death += 1;
+			pthread_mutex_unlock (&philo->geral->lock);
+			pthread_detach(philo->thread);
+		}
+
+	}
+	return (NULL);
+}
+
+time_t current_time(void)
+{
+	struct timeval times;
+
+	gettimeofday(&times, NULL);
+	return (times.tv_usec + times.tv_sec * 1000000);
+}
+
+t_philo *create_philo(t_geral *geral, int count)
+{
+	t_philo	*philo;
+
+	philo = malloc (sizeof (t_philo));
+	philo->eat = 0;
+	philo->id = count;
+	philo->geral = geral;
+	philo->last_eat = geral->t_init;
+	return (philo);
 }
